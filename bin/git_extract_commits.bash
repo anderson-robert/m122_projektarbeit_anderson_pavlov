95ddfb3f44dba4e14cf84ea9ea3cb1c4cbf6ee5a
Robert Anderson <robert.anderson@nzz.ch>
2022-07-01 16:20:05 +0200

    added initial variables to start of script

--- bin/git_extract_commits.bash
+++ bin/git_extract_commits.bash
@@ -1,3 +1,11 @@
 #!/bin/bash
+cwd=`pwd`                   # current working directory
+cd `dirname $0`             # change to the directory where the script is located
+BINDIR=`pwd`                # BINDIR: the directory where the script is located
+cd $cwd                     # return to the working directory
+BASENAME=`basename $0`      # Set the script name (without path to it)
+TMPDIR=/tmp/$BASENAME.$$    # Set a temporary directory if needed
+ETCDIR=$BINDIR/../etc       # ETCDIR is the config directory
+
 
 #Hier kommt ihr code
