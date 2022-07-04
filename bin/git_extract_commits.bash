64f64a2942bda6e996b6a9cb391748724d824501
Robert Anderson <robert.anderson@nzz.ch>
2022-07-04 14:53:12 +0200

    added flags and started the output file formating

--- bin/git_extract_commits.bash
+++ bin/git_extract_commits.bash
@@ -7,5 +7,17 @@ BASENAME=`basename $0`      # Set the script name (without path to it)
 TMPDIR=/tmp/$BASENAME.$$    # Set a temporary directory if needed
 ETCDIR=$BINDIR/../etc       # ETCDIR is the config directory
 
+while getopts b:o: flag
+do
+    case "${flag}" in
+        b) baserepo=${OPTARG};;
+        o) output_filename=${OPTARG};;
+    esac
+done
+echo "Base Repository: $baserepo";
+echo "Output Filename: $output_filename";
 
-#Hier kommt ihr code
+target_repo=$baserepo #change $baserepo later
+
+git log --date=format:'%Y%m%d' --format=format:"%ad %H %an" > ~/Desktop/test.txt
+sed -i s@^@$target_repo@ ~/Desktop/test.txt
