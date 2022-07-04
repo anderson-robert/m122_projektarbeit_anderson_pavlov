c260fb580dbd15564e63e9e25a1c3c08dd5e96ef
Robert Anderson <robert.anderson@nzz.ch>
2022-07-04 15:26:23 +0200

    added filtering for git repos inside of base repo

--- bin/git_extract_commits.bash
+++ bin/git_extract_commits.bash
@@ -7,6 +7,15 @@ BASENAME=`basename $0`      # Set the script name (without path to it)
 TMPDIR=/tmp/$BASENAME.$$    # Set a temporary directory if needed
 ETCDIR=$BINDIR/../etc       # ETCDIR is the config directory
 
+# create and populate temp dir
+cd
+cd /tmp
+mkdir $TMPDIR
+cd $TMPDIR
+touch found_repos.txt    
+cd
+cd $cwd
+
 while getopts b:o: flag
 do
     case "${flag}" in
@@ -17,7 +26,10 @@ done
 echo "Base Repository: $baserepo";
 echo "Output Filename: $output_filename";
 
-target_repo=$baserepo #change $baserepo later
+find $baserepo -name '*.git' > $TMPDIR/found_repos.txt
+sed -i -r 's|/[^/]+$||' $TMPDIR/found_repos.txt
 
-git log --date=format:'%Y%m%d' --format=format:"%ad %H %an" > ~/Desktop/test.txt
-sed -i s@^@$target_repo@ ~/Desktop/test.txt
+#target_repo=$baserepo #change $baserepo later
+
+#git log --date=format:'%Y%m%d' --format=format:"%ad %H %an" > ~/Desktop/test.txt
+#sed -i s@^@$target_repo@ ~/Desktop/test.txt
