15797d18e6505ccaa233ed09a928307846751e57
Grigory Pavlov <gpavlov@spline.ch>
2022-07-15 15:59:55 +0200

    fixed merge

--- bin/git_clone_update_repos.bash
+++ bin/git_clone_update_repos.bash
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-CONFIGFILE="../etc/repos.conf"
-CLONEDIR="../testclonedir/"
-CWD=$(pwd)
-
-echo "Cloning repositories..."
-while read -r line; do
-  echo "Working on $line"
-  git clone "$line" "$CLONEDIR" || echo "Pulling: $CLONEDIR" && cd "$CLONEDIR" && git pull 2>&1 && cd "$CWD";
-done < "$CONFIGFILE"
+# Hier kommt ihr Code
